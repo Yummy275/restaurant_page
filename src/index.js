2cc5b215b5be146f2c4c1541a50016ef795a9421
Yummy275 <htgonzalez94@yahoo.com>
2019-11-19 13:47:36 -0700

    Tabs display

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import {renderMenu} from './menu'
 import {renderContact} from './contact'
 
-renderContact();
+const renderTabs = () => {
+  let numberOfTabs = 2;
+  const tabNames = ["Menu" , "Contact"];
+  const content = document.getElementById('content');
+
+  const tabs = document.createElement('div');
+
+  for (var i = 0; i < numberOfTabs; i++) {
+    const tab = document.createElement('button');
+    tab.classList.add('tabbtn');
+    tab.textContent = tabNames[i];
+    tabs.appendChild(tab);
+  }
+
+  content.appendChild(tabs);
+}
+
+renderTabs();
+renderMenu();
